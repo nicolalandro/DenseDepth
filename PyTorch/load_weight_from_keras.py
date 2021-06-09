4947fa00446007b6722306ee8d345e84e09ac0aa
nicolalandro <nicolaxx94@live.it>
2021-06-09 10:39:34 +0200

    saved torch model weights

--- PyTorch/load_weight_from_keras.py
+++ PyTorch/load_weight_from_keras.py
@@ -86,6 +86,9 @@ for name, param in pytorch_model.named_parameters():
 pytorch_model.load_state_dict(keras_state_dict)
 pytorch_model.eval()
 
+#print('save torch weights...')
+#torch.save(pytorch_model.state_dict(),'nyu.pth')
+#print('...end save torch weights')
 
 def my_DepthNorm(x, maxDepth):
     return maxDepth / x
